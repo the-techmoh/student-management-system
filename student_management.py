edb9c8f3ba38073e0da39c97373759a0980853c6
Mohammed Yahaya <muhammedyahaya200@gmail.com>
2026-02-18 17:21:09 +0100

    Prepare project for deployment

--- student_management.py
+++ student_management.py
@@ -229,5 +229,9 @@ def logout():
 # RUN APP
 # -----------------------------
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
